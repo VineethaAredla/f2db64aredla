880f4b874c277eded73fb344bab953d90cc2c4b9
Vineetha Aredla <112147217+VineethaAredla@users.noreply.github.com>
2022-11-04 10:17:42 -0500

    changes are made to repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# f2wb64aredla
+
+Its fun generating web pages on the server 
+
+[Render](https://f2wb64aredla.onrender.com)
+
+Dog
+dog_Name(String) age(number) price(number)
+
---
+++ routes/Dog.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+class Dog {
+constructor(dog_Name, age, price){
+  this.dog_Name=dog_Name;
+  this.age=age;
+  this.price=price;
+  
+}
+}
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let D1=new Dog('Alaskan Malamute',5,12000);
+  let D2=new Dog('Akita',9,10000);
+  let D3=new Dog('American Leopard Hound',12,13000);
+
+  res.render('Dog', { title: 'Search Results Dog',Dog : [D1,D2,D3] });
+});
+
+
+module.exports = router;
