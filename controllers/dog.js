a65efade15e75d459717da8dc1067ea9e1d46ac8
Vineetha Aredla <112147217+VineethaAredla@users.noreply.github.com>
2022-11-17 15:41:06 -0600

    update file is added

--- controllers/dog.js
+++ controllers/dog.js
@@ -131,3 +131,16 @@ exports.dog_create_Page = function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+// Handle building the view for updating a dog.
+// query provides the id
+exports.dog_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await watch.findById(req.query.id)
+        res.render('dogupdate', { title: 'dog Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
